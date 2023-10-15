777bd9b553207619d6e4d788f4a2a92f01909028
DefriDwamn <Yakovnova@gmail.com>
2023-10-16 00:49:39 +0300

    DZ4 add

--- DZ4/ss.cpp
+++ DZ4/ss.cpp
@@ -1,41 +1,83 @@
+#include <stdexcept>
 #include <iostream>
 #include <string>
 
 int toDecimal(std::string num, int base) {
-  int digits = 0, sum = 0;
-
-  digits = num.length();
+  int digits = num.length(), sum = 0;
 
   for (size_t i = 0; i < digits; i++) {
     char curr = num[i];
-    if (curr >= 'A' && curr <= 'F') {
+    int currToInt;
+    if (curr >= 'A' && curr <= 'Z') {
       curr = (int)(curr - 16) + 9;
-    } else if (curr >= 'a' && curr <= 'f') {
-      curr = (int)(curr - 48) + 9;
-    }
-    int currToInt = curr - '0';
+      currToInt = curr - '0';
+    } else if (curr >= '0' && curr <= '9') {
+      currToInt = curr - '0';
+    } else
+      throw std::invalid_argument("Wrong Num");
+
+    if (currToInt >= base) throw new std::invalid_argument("Wrong Num");
+
     sum += std::pow(base, digits - i - 1) * currToInt;
   }
 
   return sum;
 }
 
-std:::string fromDecimal(int num, int base) {
-  std::string digits;
-  for (; num > 0; num = num / 2) {
-    
+std::string fromDecimal(int num, int base) {
+  std::string tresult;
+  if (num < base) {
+    if (num >= 10 && num <= 36) {
+      char n1, n2;
+      n1 = char((num - 9) + 16 + '0');
+      n2 = char(num + '0');
+      if (n1 >= 'A' && n1 <= 'Z') {
+        tresult += n1;
+      } else
+        tresult += n2;
+
+      return tresult;
+    } else
+      return std::to_string(num);
   }
-  return res;
+
+  for (; num > 0; num = num / base) {
+    int ost = num % base;
+    if (ost >= 10 && ost <= 36) {
+      char n1, n2;
+      n1 = char((ost - 9) + 16 + '0');
+      n2 = char(ost + '0');
+      if (n1 >= 'A' && n1 <= 'Z') {
+        tresult += n1;
+      } else
+        tresult += n2;
+
+    } else
+      tresult += std::to_string(ost);
+  }
+  std::string result(tresult.rbegin(), tresult.rend());
+  return result;
 }
 
 int main() {
+  setlocale(0, "");
   std::string num;
   int base, tobase;
-  std::cout << "Введите: число | его систему счисления | систему перевода: ";
-  std::cin >> num >> base >> tobase;
-  int temp = toDecimal(num, base);
-  if (tobase == 10)
-    std::cout << temp;
-  else
-    std::cout << fromDecimal(temp, tobase);
+
+  while (1) {
+    try {
+      std::cout
+          << "Введите: число | его систему счисления | систему перевода: ";
+      std::cin >> num >> base >> tobase;
+      int temp = toDecimal(num, base);
+      if (tobase == 10)
+        std::cout << temp << '\n';
+      else {
+        std::string fd = fromDecimal(temp, tobase);
+        std::cout << fd << '\n';
+      }
+    } catch (std::exception& e) {
+      std::cout << e.what() << '\n';
+    }
+  }
 }
