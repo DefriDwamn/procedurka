7af394c5a3842af4ac248cbc9be260594813ab55
DefriDwamn <Yakovnova@gmail.com>
2023-10-28 03:20:33 +0300

    DZ5 add

--- DZ5/4_20.cpp
+++ DZ5/4_20.cpp
@@ -1,8 +1,43 @@
+#include <bitset>
+#include <cmath>
 #include <iostream>
 
-int main() {    
-    size_t n;
-    std::cout << "Укажите n:";    
-    std::cin >> n;
-    
+std::string* to4(std::bitset<32>* bs, size_t st) {
+  std::string* s4 = new std::string[st];
+  for (size_t i = 0; i != st; ++i) {
+    std::string s;
+    for (int j = 31; j > 0; j = j - 2) {
+      int l = bs[i][j];
+      int r = bs[i][j - 1];
+      if (l && r)
+        s += "3";
+      else if (l && !r)
+        s += "2";
+      else if (!l && r)
+        s += "1";
+      else
+        s += "0";
+    }
+    s4[i] = s;
+  }
+  return s4;
+}
+
+int main() {
+  size_t n;
+  std::cout << "Укажите n для диапазона [1:n]:";
+  std::cin >> n;
+  if (n < 1 || n > 4294967295) {
+    std::cout << "Указан неверный диапазон!\n";
+    return 0;
+  }
+  std::bitset<32>* bs = new std::bitset<32>[n + 1];
+  for (size_t i = 0; i <= n; ++i) {
+    bs[i] = i;
+  }
+  std::string* s4 = to4(bs, n + 1);
+  for (size_t i = 0; i != n + 1; ++i) {
+    std::cout << s4[i] << '\n';
+  }
+  delete[] bs;
 }
---
+++ DZ5/4_40.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+// 1 3 2 1 3 3 3 3 1 4 5 3 2 - [3 2 1]+[3 1 4]+[5 3 2] = 3
+size_t countDecrSeq(std::vector<int>& seq) {
+  size_t count = 0;
+  size_t c = 0, prevC;
+  if (seq.size() < 2) return 0;
+  for (auto it = ++seq.begin(); it != seq.end(); ++it) {
+    prevC = c;
+    if (*it < *(it - 1))
+      ++c;
+    else
+      c = 0;
+    if ((c != prevC) && c == 0) ++count;
+  }
+  if (c != 0) ++count;
+  return count;
+}
+
+int main() {
+  std::cout << "Введите последовательность\n:";
+  std::vector<int> vec;
+  int n;
+  while (std::cin >> n) {
+    vec.push_back(n);
+  }
+  std::cout << "Кол-во уб. послед.: " << countDecrSeq(vec);
+}
